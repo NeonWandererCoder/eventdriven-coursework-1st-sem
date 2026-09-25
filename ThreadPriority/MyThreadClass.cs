28d333fe652992e0d2f6f1f8fa60325bf61023fa
EnriquezWandererCoder <147671506+NeonWandererCoder@users.noreply.github.com>
2026-09-25 17:42:53 +0900

    04 Task Performance 1 - ARG
    04 Task Performance 1 - ARG

---
+++ ThreadPriority/MyThreadClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+public class MyThreadClass
+{
+    public static void Thread1()
+    {
+        // loops 3 times (Process = 0 to 2), matching the expected output in Figure 2
+        for (int loopCount = 0; loopCount <= 2; loopCount++)
+        {
+            Thread thread = Thread.CurrentThread;
+            Console.WriteLine("Name of Thread: " + thread.Name + " Process = " + loopCount);
+
+            // suspend the current thread for 0.5 seconds
+            Thread.Sleep(500);
+        }
+    }
+
+    public static void Thread2()
+    {
+        // loops 6 times (Process = 0 to 5)
+        for (int loopCount = 0; loopCount <= 5; loopCount++)
+        {
+            Thread thread = Thread.CurrentThread;
+            Console.WriteLine("Name of Thread: " + thread.Name + " Process = " + loopCount);
+
+            // suspend the current thread for 1.5 seconds
+            Thread.Sleep(1500);
+        }
+    }
+}
---
+++ ThreadPriority/frmTrackThread.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace ThreadPriority
+{
+    public partial class frmTrackThread : Form
+    {
+        public frmTrackThread()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("-Thread Starts-");
+
+            // threadA and threadC use the fast method (Thread1)
+            Thread threadA = new Thread(MyThreadClass.Thread1);
+            threadA.Name = "Thread A";
+            threadA.Priority = System.Threading.ThreadPriority.Highest;
+
+            Thread threadC = new Thread(MyThreadClass.Thread1);
+            threadC.Name = "Thread C";
+            threadC.Priority = System.Threading.ThreadPriority.AboveNormal;
+
+            // threadB and threadD use the slower method (Thread2)
+            Thread threadB = new Thread(MyThreadClass.Thread2);
+            threadB.Name = "Thread B";
+            threadB.Priority = System.Threading.ThreadPriority.Normal;
+
+            Thread threadD = new Thread(MyThreadClass.Thread2);
+            threadD.Name = "Thread D";
+            threadD.Priority = System.Threading.ThreadPriority.BelowNormal;
+
+            // start all 4 threads so they run at the same time
+            threadA.Start();
+            threadB.Start();
+            threadC.Start();
+            threadD.Start();
+
+            // wait here until every thread is fully done
+            threadA.Join();
+            threadB.Join();
+            threadC.Join();
+            threadD.Join();
+
+            Console.WriteLine("-End of Thread-");
+
+            // only update the label once all 4 threads have actually finished
+            lblStatus.Text = "-End of Thread-";
+        }
+    }
+}
